59c16da1021a6dd7c6035ba48b6c0ef1356c2b9b
Bagniofox2010 <samfoltin@gmail.com>
2022-10-01 21:49:35 +0100

    Update streamlit_app.py
    Add input box

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ st.dataframe(fruits_to_show)
 # Display FruityVice API Response
 st.header('Fruityvice Fruit Advice!')
 fruit_choice = st.text_input('What fruit would you like information about?', 'Kiwi')
-st.write('The user netered, fruit_choice')
+st.write('The user entered, fruit_choice')
          
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
@@ -37,3 +37,7 @@ my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 st.header("The fruit load list contains:")
 st.dataframe(my_data_rows)
+
+# Allow the end user to add a fruit to the list
+add_my_fruit = st.text_input('What fruit would you like to add to the list?')
+st.write('Thanks for adding, add_my_fruit')
