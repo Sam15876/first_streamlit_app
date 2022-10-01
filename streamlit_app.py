ed4b9bee59dcf9d3d0fcc65a0f4791bd8e837f74
Bagniofox2010 <samfoltin@gmail.com>
2022-10-01 20:32:39 +0100

    Update streamlit_app.py
    Parse response as json before displaying

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,27 @@
-import streamlit
+import streamlit as st
 import pandas
+import requests
 
-streamlit.title('My Parents New Healthy Diner')
+st.title('My Parents New Healthy Diner')
 
-streamlit.header('🥣 Breakfast Menu')
-streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🐔 Kale, Spinarch & Rocket Smoothie')
-streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')
+st.header('🥣 Breakfast Menu')
+st.text('🥗 Omega 3 & Blueberry Oatmeal')
+st.text('🐔 Kale, Spinarch & Rocket Smoothie')
+st.text('🥑🍞 Hard-Boiled Free-Range Egg')
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 # Load fruit list dataset
 my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-fruits_selected = streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_selected = st.multiselect('Pick some fruits:', list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
-streamlit.dataframe(fruits_to_show)
+st.dataframe(fruits_to_show)
+
+# Display FruityVice API Response
+st.header('Fruityvice Fruit Advice!')
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response.json())
